e3e77b237ab750332cb740160b421683849d7fad
Israel Mateos Aparicio <israel4720@gmail.com>
2023-10-14 20:16:53 +0200

    Añadida obtención de códigos de estados

--- src/data/get_datasets.py
+++ src/data/get_datasets.py
@@ -1,6 +1,9 @@
+import bs4
+import requests
 from dotenv import find_dotenv, load_dotenv
 
 load_dotenv(find_dotenv())
+
 import kaggle
 
 
@@ -11,5 +14,26 @@ def get_gun_violence_data():
     )
 
 
+def get_us_states_codes():
+    url = "https://en.wikipedia.org/wiki/ISO_3166-2:US"
+    request = requests.get(url)
+    soup = bs4.BeautifulSoup(request.text, "html.parser")
+    table = soup.find("table")
+    table_body = table.find("tbody")
+
+    states = {}
+    for row in table_body.find_all("tr"):
+        # If the row is a header, skip it
+        if row.find("th"):
+            continue
+        cells = row.find_all("td")
+        state_code = cells[0].find("span").text[-2:]
+        state_name = cells[1].find("a").text
+        states[state_name] = state_code
+
+    return states
+
+
 def get_datasets():
     get_gun_violence_data()
+    get_us_states_codes()
