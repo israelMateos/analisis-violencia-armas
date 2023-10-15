8bef4f9cf042bb4a669ff105ac46e15049fd6e8e
Israel Mateos Aparicio <israel4720@gmail.com>
2023-10-15 12:47:39 +0200

    Añadida obtención de dataset de pobreza

--- src/data/get_datasets.py
+++ src/data/get_datasets.py
@@ -1,6 +1,8 @@
 import bs4
+import pandas as pd
 import requests
 from dotenv import find_dotenv, load_dotenv
+from selenium import webdriver
 
 load_dotenv(find_dotenv())
 
@@ -16,8 +18,8 @@ def get_gun_violence_data():
 
 def get_us_states_codes():
     url = "https://en.wikipedia.org/wiki/ISO_3166-2:US"
-    request = requests.get(url)
-    soup = bs4.BeautifulSoup(request.text, "html.parser")
+    response = requests.get(url)
+    soup = bs4.BeautifulSoup(response.text, "html.parser")
     table = soup.find("table")
     table_body = table.find("tbody")
 
@@ -27,6 +29,10 @@ def get_us_states_codes():
         if row.find("th"):
             continue
         cells = row.find_all("td")
+        subcategory = cells[2].text.replace("\n", "")
+        # If the row is not a state or a district, skip it
+        if subcategory != "State" and subcategory != "District":
+            continue
         state_code = cells[0].find("span").text[-2:]
         state_name = cells[1].find("a").text
         states[state_name] = state_code
@@ -34,6 +40,87 @@ def get_us_states_codes():
     return states
 
 
+def get_poverty_data():
+    site = "https://www.povertyusa.org/data"
+    states = get_us_states_codes()
+    years = ["2015", "2016", "2017", "2018"]
+
+    # The sites uses JavaScript to load the data, so we need to use Selenium
+    options = webdriver.ChromeOptions()
+    options.add_argument("--ignore-certificate-errors")
+    options.add_argument("--incognito")
+    options.add_argument("--headless")
+
+    driver = webdriver.Chrome(options=options)
+
+    data = []
+    for year in years:
+        for state_name, state_code in states.items():
+            url = site + f"/{year}/{state_code}"
+            driver.get(url)
+            # Once the page is loaded, we can use BeautifulSoup to parse the HTML
+            page_source = driver.page_source
+            soup = bs4.BeautifulSoup(page_source, "html.parser")
+
+            row = [int(year), state_name]
+            # Population
+            row.append(
+                int(
+                    soup.find("div", {"class": "simple-stat population stat"})
+                    .find("span", {"class": "stat"})
+                    .find("span")["data-value"]
+                )
+            )
+            # Population in poverty
+            row.append(
+                int(
+                    soup.find("div", {"class": "simple-stat in-poverty stat"})
+                    .find("span", {"class": "stat"})
+                    .find("span")["data-value"]
+                )
+            )
+            # Poverty rate
+            poverty_rate_section = soup.find(
+                "div", {"class": "simple-stat poverty-rate stat"}
+            )
+            row.append(
+                None  # Some states and years don't have data for this
+                if poverty_rate_section is None
+                else poverty_rate_section.find("span", {"class": "stat"}).find("span")[
+                    "data-value"
+                ]
+            )
+            # Median household income, deep poverty rate, median rent, unemployment rate,
+            # without health insurance and supplemental poverty measure
+            stats = soup.find_all("h2", {"class": "stat h1 font-weight--light"})
+            for stat in stats:
+                # There are both ints and percentages
+                stat = stat.find("span")["data-value"]
+                row.append(float(stat) if "." in stat else int(stat))
+
+            data.append(row)
+
+    driver.quit()
+
+    df = pd.DataFrame(
+        data,
+        columns=[
+            "year",
+            "state",
+            "population",
+            "in_poverty",
+            "poverty_rate",
+            "median_household_income",
+            "deep_poverty_rate",
+            "median_rent",
+            "unemployment_rate",
+            "without_health_insurance",
+            "supplemental_poverty_measure",
+        ],
+    )
+    df.to_csv("data/raw/poverty_data.csv", index=False)
+
+
 def get_datasets():
     get_gun_violence_data()
-    get_us_states_codes()
+    get_poverty_data()
